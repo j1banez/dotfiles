5fb3c1cd34c2e546d5192506610712db7ff3995f
Jeremy Ibanez <jeremy.ibanez@protonmail.com>
2026-02-22 18:30:48 +0100

    Update setup.sh

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/j1banez/dotfiles/master/se
 ```
 
 Curl, Git, Vim and Bash must be installed.
+
+Optional dependencies (recommended based on enabled configs):
+
+- Zsh + Oh My Zsh
+- Neovim
+- Tmux
+- Ghostty
+- restic (for `bin/s3restic`)
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # Colors for output
 GRN="\033[0;32m"
 BLU="\033[0;34m"
@@ -9,7 +11,6 @@ PUR="\033[1;35m"
 NON="\033[0m"
 
 # Gets the absolute path of the dotfiles directory
-# Eg. ~/.dotfiles
 DOTFILES_DIR="$HOME/.dotfiles"
 
 # Prints a title
@@ -19,87 +20,105 @@ title() { echo "\n"$BLU"$@:"$NON ; }
 info() { echo $PUR": $@"$NON ; }
 
 # Prints a warning
-warning() { echo $RED"warning: $@"$NON ; }
+warning() { echo $RED"! $@"$NON ; }
 
 # Prints && executes a command
 exe() { echo $YEL"> $@"$NON ; "$@" ; }
 
 # Tries to git clone
-trygit() { exe git clone $@ 2>/dev/null || warning "git repository already exists" ; }
+trygit() { exe git clone "$@" 2>/dev/null || warning "git repository already exists" ; }
 
-# Saves a config (.*rc) file if already exists
-savefile() {
-	if [ -f $HOME/$@ ]; then # If file already exists
-		mv $HOME/$@ $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")
-		info "old $@ saved in $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")"
-	fi
-}
+#
+# Setup start
+#
 
-# Saves a config directory if already exists
-savedir() {
-	if [ -d $HOME/$@ ]; then # If directory already exists
-		mv $HOME/$@ $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")
-		info "old $@ saved in $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")"
-	fi
-}
-
-# If ~/dotfiles already exists, delete it and re git clone it
-if [ -d $DOTFILES_DIR ]; then
-	rm -rf $DOTFILES_DIR
-fi
-trygit https://github.com/lenemson/dotfiles $DOTFILES_DIR
-# Creates an `oldrc` directory to store old config files/directories
-# This directory will be ignored by git
-mkdir -p $DOTFILES_DIR/oldrc
+title ".dotfiles"
+# Clones dotfiles if needed
+trygit https://github.com/j1banez/dotfiles "$DOTFILES_DIR"
 # (-(-.(-.-).-)-)
-echo $GRN"# Dotfiles git repository path: $DOTFILES_DIR"$NON
+info "dotfiles git repository path: $DOTFILES_DIR"
 
 # Git
 title "Git"
 # Symlinks git config files
-savefile .gitconfig
-exe ln -s $DOTFILES_DIR/.gitconfig $HOME
-savefile .gitignore_global
-exe ln -s $DOTFILES_DIR/.gitignore_global $HOME
+exe ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+exe ln -sf "$DOTFILES_DIR/.gitignore_global" "$HOME/.gitignore_global"
 info "Git config done!"
 
 # Vim
 title "Vim"
 # Symlinks .vim directory
-savedir .vim
-exe ln -s $DOTFILES_DIR/.vim $HOME
+exe ln -sfn "$DOTFILES_DIR/.vim" "$HOME/.vim"
 # Install/Clone Vundle into .vim/bundle
-trygit https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+trygit https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 # Symlinks vimrc
-savefile .vimrc
-exe ln -s $HOME/.vim/vimrc $HOME/.vimrc
+exe ln -sf "$HOME/.vim/vimrc" "$HOME/.vimrc"
 # Install vim plugin via Vundle
 exe vim +PluginInstall +qall
 info "Vim config done!"
 
+# Neo Vim
+title "Neo Vim"
+mkdir -p "$HOME/.config/nvim"
+exe ln -sf "$DOTFILES_DIR/.config/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+info "Neo Vim config done!"
+
 # Bash
 title "Bash"
 # Download git-prompt script
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+if [ ! -f "$HOME/.git-prompt.sh" ]; then
+    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$HOME/.git-prompt.sh"
+fi
 # Symlinks bashrc
-savefile .bashrc
-exe ln -s $DOTFILES_DIR/.bashrc $HOME
+exe ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 info "Bash config done!"
+
+# Zsh
+title "Zsh"
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    exe ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+    exe ln -sfn "$DOTFILES_DIR/.oh-my-zsh/custom" "$HOME/.oh-my-zsh/custom"
+    info "Zsh config done!"
+else
+    warning "Oh My Zsh not found. Install it manually first: https://ohmyz.sh"
+    warning "Then run setup.sh again to link ~/.zshrc and ~/.oh-my-zsh/custom"
+fi
 
 # Tmux
 title "Tmux"
-# Symlink tmux.conf
-savefile .tmux.conf
-exe ln -s $DOTFILES_DIR/.tmux.conf $HOME
+exe ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
 info "Tmux config done!"
 
+# Ghostty
+title "Ghostty"
+mkdir -p "$HOME/.config/ghostty"
+exe ln -sf "$DOTFILES_DIR/.config/ghostty/config" "$HOME/.config/ghostty/config"
+info "Ghostty config done!"
+
+# s3restic local config
+title "s3restic"
+mkdir -p "$HOME/.config/s3restic"
+if [ ! -f "$HOME/.config/s3restic/env" ]; then
+    cat > "$HOME/.config/s3restic/env" <<'EOF'
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export BUCKET_NAME=
+EOF
+    chmod 600 "$HOME/.config/s3restic/env"
+    info "Created $HOME/.config/s3restic/env"
+fi
+info "s3restic config done!"
+
+title "bin"
 # Create bin directory
-mkdir -p $HOME/bin
-
+mkdir -p "$HOME/bin"
 # Copy programs into home bin directory
-cp -r $DOTFILES_DIR/bin/* $HOME/bin
+set -- "$DOTFILES_DIR/bin/"*
+if [ -e "$1" ]; then
+    cp -r "$@" "$HOME/bin"
+    info "Some programs has been placed in ~/bin"
+else
+    warning "No files found in $DOTFILES_DIR/bin"
+fi
 
-info "\nSome programs has been placed in ~/bin"
-
-cd $HOME
-exec bash
+echo $GRN"\nSetup finished. Open a new terminal or run: source ~/.zshrc"$NON
